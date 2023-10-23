e3648704cbfe32b312c49459bf24a9b8ec521e37
LucianS7 <luciansabau87@yahoo.com>
2023-10-23 15:20:31 +0800

    some tests

--- test.js
+++ test.js
@@ -1,13 +1,32 @@
-var funcs = [];
-// let's create 3 functions
-for (let i = 0; i < 3; i++) {
-  // and store them in funcs
-  funcs[i] = function() {
-    // each should log its value.
-    console.log("My value: " + i);
-  };
-}
-for (var j = 0; j < 3; j++) {
-  // and now let's run each one to see
-  funcs[j]();
-}
+// var funcs = [];
+// // let's create 3 functions
+// for (let i = 0; i < 3; i++) {
+//   // and store them in funcs
+//   funcs[i] = function() {
+//     // each should log its value.
+//     console.log("My value: " + i);
+//   };
+// }
+// for (var j = 0; j < 3; j++) {
+//   // and now let's run each one to see
+//   funcs[j]();
+// }
+
+
+notes = [
+  {
+    id: 1,
+    body: "Note 1"
+    },
+    {
+      id:2,
+      body: "Note 2"
+    },
+
+]
+
+const currentNoteId = (false && 3) || "";
+
+console.log(currentNoteId)
+
+
